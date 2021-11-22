6f03b54900305b56df31292fada6b13c8592f297
BoyK99 <blgkik@hotmail.com>
2021-11-22 23:49:46 +0100

    2e commit

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport"3 content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Afspraak inplannen</title>
     </head>
     <body>
         <div>
             <form action="" method="get" style="display: inline;">
-                Naam: <input type="text" name="surName"> Achternaam: <input type="text" name="lastName">
+                <label>Naam:</label> <input type="text" name="surName"> Achternaam: <input type="text" name="lastName">
                 Telefoonnummer: <input type="tel" name="phoneNumber">
                 Email Adres: <input type="email" name="emailAdress">
                 Datum en tijd: <input type="date" name="dateForm"> <input type="time" name="timeForm">
-                Restaurant locatie: <input type="" name="Locatie">
+                Restaurant locatie: <input type="text" name="Locatie">
                 Aantal personen: <input type="number">
                 <!-- Tafel (intern)-->
                 Notitie: <input type="text">

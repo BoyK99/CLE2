a6c2737936efaba14f72f214f7d66121c1b93640
BoyK99 <blgkik@hotmail.com>
2021-11-22 22:51:37 +0100

    eerste commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"3 content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Afspraak inplannen</title>
+    </head>
+    <body>
+        <div>
+            <form action="" method="get" style="display: inline;">
+                Naam: <input type="text" name="surName"> Achternaam: <input type="text" name="lastName">
+                Telefoonnummer: <input type="tel" name="phoneNumber">
+                Email Adres: <input type="email" name="emailAdress">
+                Datum en tijd: <input type="date" name="dateForm"> <input type="time" name="timeForm">
+                Restaurant locatie: <input type="" name="Locatie">
+                Aantal personen: <input type="number">
+                <!-- Tafel (intern)-->
+                Notitie: <input type="text">
+                <input value="Verzend" type="submit">
+            </form>
+        </div>
+    </body>
+</html>

ccc93bc51d36da4902fd3d04bbbb8023b3a414df
BoyK99 <blgkik@hotmail.com>
2022-01-04 01:31:46 +0100

    3e commiot

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
+<?php
+?>
 <!doctype html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Afspraak inplannen</title>
+        <title>Homepage GYS</title>
     </head>
     <body>
-        <div>
-            <form action="" method="get" style="display: inline;">
-                <label>Naam:</label> <input type="text" name="surName"> Achternaam: <input type="text" name="lastName">
-                Telefoonnummer: <input type="tel" name="phoneNumber">
-                Email Adres: <input type="email" name="emailAdress">
-                Datum en tijd: <input type="date" name="dateForm"> <input type="time" name="timeForm">
-                Restaurant locatie: <input type="text" name="Locatie">
-                Aantal personen: <input type="number">
-                <!-- Tafel (intern)-->
-                Notitie: <input type="text">
-                <input value="Verzend" type="submit">
-            </form>
-        </div>
+        <img src="img/logo.svg">
+        <form action="reserve.php">
+            <button>Maak Afspraak</button>
+        </form>
     </body>
 </html>
